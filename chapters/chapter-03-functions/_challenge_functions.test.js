ba57e392b45fd2b17209fbd06ca6da8c6fc3f2ee
cfurley <caleb.furley@gmail.com>
2025-12-10 17:23:57 -0600

    Implemented Tests for Challenge 2 minMax().

--- chapters/chapter-03-functions/_challenge_functions.test.js
+++ chapters/chapter-03-functions/_challenge_functions.test.js
@@ -1,5 +1,11 @@
-import { getCelsius } from "./_challenge_functions.js";
+import { getCelsius, minMax } from "./_challenge_functions.js";
 
 // Testing for Challenge 1.
 console.log( getCelsius(32) );
 console.log( getCelsius(212) );
+console.log( getCelsius(98) );
+
+// Testing for Challenge 2.
+console.log( minMax([1,2,3,4,5]) );
+console.log( minMax([15,6,7,12,18]) );
+console.log( minMax([-100,1,2,57,94,96,32,8]) );
