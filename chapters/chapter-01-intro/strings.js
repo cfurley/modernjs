297eddc20366924c1304f35d93516810dcd721a7
cfurley <caleb.furley@gmail.com>
2025-12-10 10:37:59 -0600

    Examples for Strings.

---
+++ chapters/chapter-01-intro/strings.js
@@ -0,0 +1,13 @@
+let x;
+
+const s = 'Hello World';
+
+x = s.trim();
+x = s.indexOf('o');
+x = s.slice(1,5);
+x = s.substring(1,5);
+x = s.replace('World', 'Mundo');
+x = s.includes('Hello');
+x = s.split(' ');
+
+console.log(x);
