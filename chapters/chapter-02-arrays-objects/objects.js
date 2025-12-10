0337aeaba5e6b6de481786b46762b630de23569a
cfurley <caleb.furley@gmail.com>
2025-12-10 13:12:03 -0600

    Spread Operator Example with Sets.

--- chapters/chapter-02-arrays-objects/objects.js
+++ chapters/chapter-02-arrays-objects/objects.js
@@ -22,3 +22,11 @@ const user2 = {
   }
 }
 console.log(user2.name, user2.address);
+
+/**
+ * Spread Operator for Objects
+ */
+const x = { a:1, b:2 };
+const y = { c:3, d:4 };
+let z = {...x, ...y};
+console.log(z);
