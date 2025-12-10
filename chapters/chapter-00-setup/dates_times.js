c6068d09a206db580c89e27ef2043d928f69c9f8
cfurley <caleb.furley@gmail.com>
2025-12-10 11:44:31 -0600

    Added some example functions of the Date() object.

--- chapters/chapter-00-setup/dates_times.js
+++ chapters/chapter-00-setup/dates_times.js
@@ -4,4 +4,14 @@ d = new Date(); // object
 d = d.toString(); // pure string
 d = new Date(2022, 2, 29); // month is 0 indexed
 
+let x;
+
+x = d.getDate();
+x = d.getDay();
+x = d.getFullYear();
+x = d.getMilliseconds();
+x = d.getSeconds();
+x = d.getMinutes();
+
 console.log(d);
+console.log(x);
