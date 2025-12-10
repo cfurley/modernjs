00331d763cbede84576c4e4aed22a7f92807d721
cfurley <caleb.furley@gmail.com>
2025-12-10 17:23:44 -0600

    Implemented Challenge 2 minMax().

--- chapters/chapter-03-functions/_challenge_functions.js
+++ chapters/chapter-03-functions/_challenge_functions.js
@@ -7,3 +7,16 @@
 export const getCelsius = ( F ) => {
   return ( F - 32 ) * (5/9);
 };
+
+/**
+ * Challenge 2: Create a function minMax that takes an
+ * array of numbers, and returns and object containing
+ * the minimum and maximum numbers from the input.
+ * @param numbers An array of numbers.
+ * @returns An object containing the min and max numbers. 
+ */
+export const minMax = ( numbers ) => {
+  const _min = Math.min(...numbers);
+  const _max = Math.max(...numbers);
+  return { min:_min, max:_max }; 
+};
