50a183951221b6d85457e41438ab20e8829f8cdd
cfurley <caleb.furley@gmail.com>
2025-12-10 16:21:12 -0600

    Implemented Challenge 1.

--- chapters/chapter-03-functions/_challenge_functions.js
+++ chapters/chapter-03-functions/_challenge_functions.js
@@ -1,3 +1,9 @@
 /**
- * Challenge 1
+ * Challenge 1: Create a function getCelsius which
+ * takes the temperature in F and returns it in C.
+ * @param F temperature in fahrenheit.
+ * @returns temperature in celsius.
  */
+export const getCelsius = ( F ) => {
+  return ( F - 32 ) * (5/9);
+};
