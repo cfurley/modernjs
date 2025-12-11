b245baee00670bc41b2b04d814c6d296a9979f20
cfurley <caleb.furley@gmail.com>
2025-12-10 19:12:43 -0600

    Example for Ternary Operator.

---
+++ chapters/chapter-04-control-flow/ternary_operator.js
@@ -0,0 +1,10 @@
+// ? :
+
+// Traditional If Else Statement.
+let x = 5;
+if (x > 10) console.log(true);
+else        console.log(false); // returns false.
+
+// Ternary Operator Statement.
+let y = 11;
+y > 10 ? console.log(true) : console.log(false); // returns true.
