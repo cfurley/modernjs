a34144b87fe9872d2f6a7ac04df980e67fdde645
cfurley <caleb.furley@gmail.com>
2025-12-10 19:32:35 -0600

    Implemented FizzBuzz using a switch. Which passes true to work.. odd but it works.

--- chapters/chapter-05-high-order-arrays/_challenge_fizzbuzz.js
+++ chapters/chapter-05-high-order-arrays/_challenge_fizzbuzz.js
@@ -1,7 +1,19 @@
 /**
- * FizzBuzz: 
- * @returns 
+ * FizzBuzz: Print the numbers from 1 to 100.
+ * For multiples of three, print 'Fizz' instead of the number.
+ * For multiples of five, print 'Buzz' instead of the number.
+ * For numbers which are multiples of both three and five print 'FizzBuzz'.
+ * @returns Null
  */
-export const fizzBuzz = () => {
-  return 'fizzBuzz';
+const fizzBuzz = () => {
+  for (i = 1; i <= 100; ++i) {
+    switch( true ) {
+      case i % 3 == 0 && i % 5 == 0: console.log('FizzBuzz'); break;
+      case i % 3 == 0:                console.log('Fizz'); break;
+      case i % 5 == 0:                console.log('Buzz'); break;
+      default:                        console.log(i); break;
+    }
+  }
 };
+
+fizzBuzz();
