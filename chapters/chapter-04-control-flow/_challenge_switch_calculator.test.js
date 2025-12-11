10011cdbc6e7685b24802948d325fd3b01e95065
cfurley <caleb.furley@gmail.com>
2025-12-10 19:13:58 -0600

    Implemented Testing for the calculator() for the Calculator Challenge.

---
+++ chapters/chapter-04-control-flow/_challenge_switch_calculator.test.js
@@ -0,0 +1,31 @@
+import { calculator } from "./_challenge_switch_calculator.js";
+
+console.log( calculator( 2, 2, '+' ) );
+console.log( calculator( 2, 2, '-' ) );
+console.log( calculator( 2, 2, '*' ) );
+console.log( calculator( 2, 2, '/' ) );
+console.log( calculator( 2, 2, '=' ) );
+
+console.log( calculator( 10, 10, '+' ) );
+console.log( calculator( 10, 10, '-' ) );
+console.log( calculator( 10, 10, '*' ) );
+console.log( calculator( 10, 10, '/' ) );
+console.log( calculator( 10, 10, '=' ) );
+
+console.log( calculator( -3, -3, '+' ) );
+console.log( calculator( -3, -3, '-' ) );
+console.log( calculator( -3, -3, '*' ) );
+console.log( calculator( -3, -3, '/' ) );
+console.log( calculator( -3, -3, '=' ) );
+
+console.log( calculator( 0, 0, '+' ) );
+console.log( calculator( 0, 0, '-' ) );
+console.log( calculator( 0, 0, '*' ) );
+console.log( calculator( 0, 0, '/' ) );
+console.log( calculator( 0, 0, '=' ) );
+
+console.log( calculator( 67, 67, '+' ) );
+console.log( calculator( 67, 67, '-' ) );
+console.log( calculator( 67, 67, '*' ) );
+console.log( calculator( 67, 67, '/' ) );
+console.log( calculator( 67, 67, '=' ) );
